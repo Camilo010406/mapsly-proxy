9b73a0ad3c20b844e747782bbb8053edf9350d03
Camilo Delgadillo <devs@neurolynx.tech>
2025-12-17 17:54:27 -0500

    Changed all the internal code for the endpoint

--- api/mapsly-appointment.js
+++ api/mapsly-appointment.js
@@ -1,7 +1,7 @@
 // api/mapsly-appointment.js
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -63,4 +63,4 @@ export default async function handler(req, res) {
     console.error("Mapsly proxy error:", e);
     return res.status(500).json({ ok: false, error: "Internal error" });
   }
-}
+};
